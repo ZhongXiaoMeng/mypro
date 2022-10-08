c40df2e5f7ca2b1fe83836a83e80eb9495cd0c4f
ZZY <1806807298@qq.com>
2022-10-08 09:43:34 +0800

    v1.0

---
+++ HelloWorld111.java
@@ -0,0 +1,48 @@
+package com.zhongziyan;
+
+
+public class HelloWorld111 {
+
+
+    public static void main(String[] args) {
+        // System.out.println("Hello World");
+        /*
+
+         */
+        //
+        System.out.println(666);
+        System.out.println(15.10);
+        System.out.println('\t');
+        System.out.println('a');
+        System.out.println(' ');
+        System.out.println('b');
+        System.out.println('\n');
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println('\t');
+        int a = 20;
+        char b ='a';
+        System.out.println("我爱你中国");
+        System.out.println(true);
+        System.out.println(false);
+        System.out.println(a);
+        System.out.println(b+1);
+        System.out.println(b+1);
+        /*
+        //
+         */
+//
+        /**/
+        /**/
+//
+        boolean b1=false;
+//        Boolean a =1;
+        System.out.println(10.00/0.2);
+        System.out.println(-10.001/0.2);
+        System.out.println(10/-0.2);
+        System.out.println(-10/-0.2);
+
+    }
+
+
+}
